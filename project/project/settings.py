7a2c66c5439ead9b0e6ea9f4942aa639844c98af
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-05-22 19:58:21 +0500

    бейдж rcoveralls

--- project/project/settings.py
+++ project/project/settings.py
@@ -165,6 +165,8 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+COVERALLS_REPO_TOKEN = os.environ.get('COVERALLS_REPO_TOKEN')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
