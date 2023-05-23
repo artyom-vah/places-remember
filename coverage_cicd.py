dea3dde4c84345cdd67b7916f7ee181be1021751
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-05-23 22:44:26 +0500

    бейдж coveralls-37

--- coverage_cicd.py
+++ coverage_cicd.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     print(result)
     # sys.exit(bool(failures))
     SVG = f"""
-    <svg xmlns="http://www.w3.org/2000/svg" width="196" height="70">
+    <svg xmlns="http://www.w3.org/2000/svg" width="196" height="20">
     <title>Coverage - { result }% </title>
     <defs>
         <linearGradient id="workflow-fill" x1="50%" y1="0%" x2="50%" y2="100%">
